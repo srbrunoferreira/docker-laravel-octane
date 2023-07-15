8ad35ffeb10babedb6bcaa7e963aa7832949f62a
Bruno Ferreira <srbrunoferreira@outlook.com>
2023-07-15 22:57:32 +0000

    build: basic setup of Jetstream, Octane, Telescope, Sanctum

---
+++ app/Http/Controllers/ApiTokenController.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Controller;
+
+final class ApiTokenController extends Controller {
+    public function __contruct() {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' => 'Bruno Ferreira',
+            'email' => 'brunoferreira@intern.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
+
+        // \App\Models\Team::factory([
+        //     'name' => $this->faker->unique()->company(),
+        //     'user_id' => User::factory(),
+        //     'personal_team' => true,
         // ]);
     }
 }
